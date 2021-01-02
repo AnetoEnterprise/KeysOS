ef957b808217c95c381942c5cb20ea10d3beddb9
Aneto <Kindlysertir@gmail.com>
2021-01-02 02:14:20 +0100

    Add files via upload

--- DemoForLinux.cpp
+++ DemoForLinux.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <dlfcn.h>
 #include <string>
 
 #include <jsoncpp/json/json.h>
@@ -39,11 +40,10 @@ handle=dlopen("./keysos.so", RTLD_LAZY);
 if(!handle){printf("%s", dlerror());}
 openKeysOS=(create_t*)dlsym(handle, "create");
 closeKeysOS=(destroy_t*)dlsym(handle, "destroy");
-if(!openKeysOS){cout << "%s" << dlerror();}
-if(!closeKeysOS){cout << "%s" << dlerror();}
+if(!openKeysOS){cout << dlerror() << "\n";}
+if(!closeKeysOS){cout << dlerror() << "\n";}
 ps=openKeysOS();
-string response=ps->CreateSelectKeys("Your request here");
-cout << response << "\n";
+string response=ps->CreateSelectKeys("CREATE KEYS{SOCIETY{NAME='Test',DOMAINNAME='www.test.com'}SECURITY{LEVEL=1}EXPIRATION{DATE=CURRENT_DATE,TIME='23:50'}FIND{DATA='My data to secure'}};");
 closeKeysOS(ps);
 
 //Début obtention des données XML du resultat
@@ -51,8 +51,19 @@ parsingSuccessful = reader.parse(response.c_str(), root);
 if ( !parsingSuccessful ){
 cout << reader.getFormattedErrorMessages() << std::endl;
 }else{
-valueFindXML=root["DATA"].asString();
+size_t foundWORD1=response.find("\"SUCCESS\":");
+size_t foundWORD2=response.find("\"PENDING\":");
+if(foundWORD1!=string::npos){
+valueFindXML=root["SUCCESS"].get("DATA", "NoResponseDATA" ).asString();
 cout << valueFindXML << "\n";
+}else{
+if(foundWORD2!=string::npos){
+valueFindXML=root["PENDING"].get("DATA", "NoResponseDATA" ).asString();
+cout << valueFindXML << "\n";
+}else{
+valueFindXML=root["ERROR"].get("DATA", "NoResponseDATA" ).asString();
+cout << valueFindXML << "\n";
+}}
 }
 //Fin obtention des données XML du resultat
 
--- DemoForWindows.cpp
+++ DemoForWindows.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <dlfcn.h>
 #include <string>
 
 #include <json/json.h>
@@ -41,11 +42,10 @@ handle=dlopen("./keysos.dll", RTLD_LAZY);
 if(!handle){printf("%s", dlerror());}
 openKeysOS=(create_t*)dlsym(handle, "create");
 closeKeysOS=(destroy_t*)dlsym(handle, "destroy");
-if(!openKeysOS){cout << "%s" << dlerror();}
-if(!closeKeysOS){cout << "%s" << dlerror();}
+if(!openKeysOS){cout << dlerror() << "\n";}
+if(!closeKeysOS){cout << dlerror() << "\n";}
 ps=openKeysOS();
-string response=ps->CreateSelectKeys("Your request here");
-cout << response << "\n";
+string response=ps->CreateSelectKeys("CREATE KEYS{SOCIETY{NAME='Test',DOMAINNAME='www.test.com'}SECURITY{LEVEL=1}EXPIRATION{DATE=CURRENT_DATE,TIME='23:50'}FIND{DATA='My data to secure'}};");
 closeKeysOS(ps);
 
 //Début obtention des données XML du resultat
@@ -53,8 +53,19 @@ parsingSuccessful = reader->parse(response.c_str(), response.c_str() + response.
 if ( !parsingSuccessful ){
 cout << errorJSON << "\n";
 }else{
-valueFindXML=root["DATA"].asString();
+size_t foundWORD1=response.find("\"SUCCESS\":");
+size_t foundWORD2=response.find("\"PENDING\":");
+if(foundWORD1!=string::npos){
+valueFindXML=root["SUCCESS"].get("DATA", "NoResponseDATA" ).asString();
 cout << valueFindXML << "\n";
+}else{
+if(foundWORD2!=string::npos){
+valueFindXML=root["PENDING"].get("DATA", "NoResponseDATA" ).asString();
+cout << valueFindXML << "\n";
+}else{
+valueFindXML=root["ERROR"].get("DATA", "NoResponseDATA" ).asString();
+cout << valueFindXML << "\n";
+}}
 }
 //Fin obtention des données XML du resultat
 
